2e3f745a1b7b737f9673d7782017cfa7ab003865
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2023-07-17 11:23:38 +0300

    Update pre_vpl_run.sh

--- Scripts/CPP_C/Git/pre_vpl_run.sh
+++ Scripts/CPP_C/Git/pre_vpl_run.sh
@@ -1,15 +1,73 @@
 student_files="student_folder"
+
 teacher_files="teacher_folder"
-requested_file="link.txt" #must be change tothe same name in the requsted file
-teacher_link="https://github.com/cpp-exercises-5782/notebook-a.git" #link to teacher's git repository
 
-git clone --quiet $(cat $requested_file) $student_files
-git clone --quiet $teacher_link $teacher_files
-find $student_files -type f -exec cp -n {} $teacher_files \;
-find $student_files -exec cp -nR {} $teacher_files \;
-rm -f -r $student_files
-mv  $teacher_files/* ./
+requested_file="nlink.txt" # Name of the file containing student input
+
+teacher_link="https://github.com/GuyAzoulay/orgchart-b.git" # Link to teacher's git repository
 
 
+
+mapfile -t input_lines < "$requested_file"
+
+repo_url="${input_lines[0]}"
+
+commit_hash="${input_lines[1]}"
+
+
+
+# Clone the student's repository
+
+git clone --quiet "$repo_url" "$student_files"
+
+
+
+# Clone the teacher's repository
+
+git clone --quiet "$teacher_link" "$teacher_files"
+
+
+
+# Check if the provided commit hash matches the first 7 characters of the actual commit hash in the student's repository
+
+cd "$student_files"
+
+actual_commit_hash=$(git rev-parse --short=7 HEAD)
+
+echo
+
+echo $commit_hash
+
+echo
+
+echo $actual_commit_hash
+
+echo
+
+if [[ "$commit_hash" == "$actual_commit_hash" ]]; then
+
+    echo "The student's repository has the desired commit: $commit_hash"
+
+    # Proceed with the rest of the script
+
+    cd ..
+
+    find "$student_files" -type f -exec cp -n {} "$teacher_files" \;
+
+    find "$student_files" -exec cp -nR {} "$teacher_files" \;
+
+    rm -f -r "$student_files"
+
+    mv "$teacher_files"/* ./
+
+else
+
+    echo "The student's repository does not have the desired commit: $commit_hash"
+
+    # Additional actions or validations for this case
+
+    cd ..
+
+fi
 
 
