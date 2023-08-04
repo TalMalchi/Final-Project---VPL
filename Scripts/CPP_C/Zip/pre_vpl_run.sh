174a1c01010113e6ebb7dbb381952503640aec8f
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-04 19:05:58 +0300

    Update pre_vpl_run.sh

--- Scripts/CPP_C/Zip/pre_vpl_run.sh
+++ Scripts/CPP_C/Zip/pre_vpl_run.sh
@@ -1,12 +1,16 @@
-
 student_files="student_folder"
 teacher_files="teacher_folder"
-requested_file="Notebook-a.zip" #must be change to the same name in the requested file
-teacher_link="https://github.com/cpp-exercises-5782/notebook-a.git" #link to teacher's git repository
+requested_file="OrgChart.zip" # Name of the file containing student input, must be change to the same name in the requested file
+commit_teacher="f40446d"
+teacher_link="https://github.com/itay-kar/orgchart-b_test.git" # Link to teacher's git repository
 
-unzip $requested_file -d $student_files
-git clone --quiet $teacher_link $teacher_files
-find $student_files -type f -exec cp -n {} $teacher_files \;
-find $student_files -exec cp -nR {} $teacher_files \;
-rm -f -r $student_files
-mv  $teacher_files/* ./
+unzip "$requested_file" -d "$student_files"
+git clone --quiet "$teacher_link" "$teacher_files"
+cd "$teacher_files"
+git checkout "$commit_teacher"
+cd ..
+
+find "$student_files" -type f -exec cp -n {} "$teacher_files" \;
+find "$student_files" -exec cp -nR {} "$teacher_files" \;
+rm -f -r "$student_files"
+mv "$teacher_files"/* ./
