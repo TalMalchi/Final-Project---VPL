758230ff4953c41221cd1619db12ec444d8e5f95
TalMalchi <93086649+TalMalchi@users.noreply.github.com>
2023-08-06 22:05:56 +0300

    Update vpl_evaluate.sh

--- Scripts/CPP_C/Git/vpl_evaluate.sh
+++ Scripts/CPP_C/Git/vpl_evaluate.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-bash pre_vpl_run.sh
-bash ./grade >> ans.txt
-#cat vpl_environment.sh >> vpl_execution
-echo "#!/bin/bash" >> vpl_execution
-echo "echo '<|--'" >> vpl_execution
-		echo "echo '-$(head -n -1 ans.txt)'" >> vpl_execution
-		echo "echo '--|>'" >> vpl_execution		
-		echo "echo" >> vpl_execution
-		
-echo "echo 'Grade :=>>$(grep 'Grade:' ./ans.txt | cut -d' ' -f2)'" >> vpl_execution
 
+bash pre_vpl_run.sh
+bash ./grade > ans.txt
+
+total_grade=$?
+echo $total_grade
+
+
+
+# Create the vpl_execution script
+echo "#!/bin/bash" > vpl_execution
+echo "echo '<|--'" >> vpl_execution
+echo "echo '-$(head -n -1 ans.txt)'" >> vpl_execution
+echo "echo '--|>'" >> vpl_execution
+echo "echo" >> vpl_execution
+
+
+
+# Append the total grade to vpl_execution script
+echo "echo 'Grade :=>>$total_grade'" >> vpl_execution
+
+# Make vpl_execution script executable
 chmod +x vpl_execution
 
+
+# Display the vpl_execution script
 cat vpl_execution
+
